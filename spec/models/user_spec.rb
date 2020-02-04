5998c84896571828a70f86a10f3d0255d477a0d5
Johnny Almonte <johnny243@users.noreply.github.com>
2020-02-04 00:58:43 -0400

    WIP: testing User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "with a User class" do
-    
-    it "can hash a password" do
-      p = User.hash_password("test")
+  subject {
+    described_class.new(pw_cost: 11000, version: "003", email: "sn@testing.com")
+  }
 
-      expect(p).not_to eql("test")
-    end
+  describe "serializable_hash" do
+    let(:hash) {
+      subject.serializable_hash
+    }
 
-    it "can check a password hash" do
-      p = User.hash_password("test")
-      check = User.test_password("test", p)
+    let(:hash_keys) {
+      ["uuid", "email"].sort
+    }
 
-      expect(check).to eql(true)
-    end
-
-    it "creates a different hash for different users" do
-      p = User.hash_password("test")
-      p2 = User.hash_password("test")
-
-      expect(p).not_to eql(p2)
-    end
-
-    it "validates same passwords for different users" do
-      p = User.hash_password("test")
-      p2 = User.hash_password("test")
-
-      check1 = User.test_password("test", p)
-      expect(check1).to eql(true)
-
-      check2 = User.test_password("test", p2)
-      expect(check2).to eql(true)
+    specify do
+      expect(hash.count).to eq 2
+      expect(hash.keys.sort).to contain_exactly(*hash_keys)
     end
   end
 
-  context "with a single user" do
-    it "orders items chronologically" do
-      u = User.create!
-      item1 = u.items.create!
-      sleep(1)
-      item2 = u.items.create!
-      expect(u.reload.items).to eq([item2, item1])
+  describe "auth_params" do
+    specify do
+      auth_params = subject.auth_params
+      expect(auth_params.keys).to contain_exactly(:pw_cost, :version, :identifier)
+    end
+
+    specify do
+      subject.pw_nonce = "some nonce"
+
+      auth_params = subject.auth_params
+      expect(auth_params.keys).to contain_exactly(:pw_cost, :version, :identifier, :pw_nonce)
+    end
+
+    specify do
+      subject.pw_salt = "some salt"
+
+      auth_params = subject.auth_params
+      expect(auth_params.keys).to contain_exactly(:pw_cost, :version, :identifier, :pw_salt)
+    end
+
+    specify do
+      subject.pw_func = "some function"
+
+      auth_params = subject.auth_params
+      expect(auth_params.keys).to contain_exactly(:pw_cost, :version, :identifier, :pw_func, :pw_alg, :pw_key_size)
     end
   end
 
+  describe "bytes_to_megabytes" do
+    specify do
+      expect(subject.bytes_to_megabytes(1000000)).to eq "0.95MB"
+    end
+  end
 end
