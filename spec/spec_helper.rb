54cda8226be7cf8a8a0a809959f43ded19a5cc06
Johnny Almonte <johnny243@users.noreply.github.com>
2020-02-13 10:54:11 -0400

    Suppress console output during RSpec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,4 +96,18 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # Suppress console output during RSpec tests
+  original_stderr = $stderr
+  original_stdout = $stdout
+
+  config.before(:all) do
+    $stderr = File.open(File::NULL, 'w')
+    $stdout = File.open(File::NULL, 'w')
+  end
+
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
 end
