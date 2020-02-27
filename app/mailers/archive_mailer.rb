25a6d099c12fe5319d8305532f5a658aa1d1301d
Mo Bitar <me@bitar.io>
2020-02-27 10:34:32 -0600

    Email backups add account email to body

--- app/mailers/archive_mailer.rb
+++ app/mailers/archive_mailer.rb
@@ -4,9 +4,15 @@ class ArchiveMailer < ApplicationMailer
   def data_backup(user_id)
     user = User.find(user_id)
     date = Date.today
-
-    data = {:items => user.items.where(:deleted => false), :auth_params => user.auth_params}
-    attachments["SN-Data-#{date}.txt"] = {:mime_type => 'application/json', :content => JSON.pretty_generate(data.as_json({})) }
-    mail(to: user.email, subject: "Data Backup for #{date}")
+    data = {
+      items: user.items.where(deleted: false),
+      auth_params: user.auth_params
+    }
+    attachments["SN-Data-#{date}.txt"] = {
+      mime_type: 'application/json',
+      content: JSON.pretty_generate(data.as_json({}))
+    }
+    @email = user.email
+    mail(to: @email, subject: "Data Backup for #{date}")
   end
 end
