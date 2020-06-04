1e79933ee0a91c7e17d5c5f6b4b8fb8a5c14b383
Karol Sójko <karolsojko@gmail.com>
2020-06-04 17:16:21 +0200

    Fix 404 error message format

--- app/controllers/api/revisions_controller.rb
+++ app/controllers/api/revisions_controller.rb
@@ -9,7 +9,7 @@ class Api::RevisionsController < Api::ApiController
     begin
       item = current_user.items.find(params[:item_id])
     rescue ActiveRecord::RecordNotFound
-      return render json: { error: 'Item not found' }, status: :not_found
+      return render json: { error: { message: 'Item not found' } }, status: :not_found
     end
 
     render json: item.revisions.where(created_at: User::REVISIONS_RETENTION_DAYS.days.ago..DateTime::Infinity.new)
