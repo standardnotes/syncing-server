81de7675599afb700839fc377e082fb2c69c87ff
Karol Sójko <karolsojko@gmail.com>
2020-06-05 18:49:56 +0200

    Add checking for original item of the duplicate

--- app/jobs/duplicate_revisions_job.rb
+++ app/jobs/duplicate_revisions_job.rb
@@ -1,10 +1,19 @@
 class DuplicateRevisionsJob < ApplicationJob
   def perform(item_id)
     item = Item.find(item_id)
-    original_item_revisions = ItemRevision.where(item_uuid: item.duplicate_of).pluck(:revision_uuid)
 
-    original_item_revisions.each do |revision_uuid|
-      ItemRevision.create(item_uuid: item_id, revision_uuid: revision_uuid)
+    existing_original_item = Item
+      .where(uuid: item.duplicate_of, user_uuid: item.user_uuid)
+      .first
+
+    if existing_original_item
+      original_item_revisions = existing_original_item
+        .item_revisions
+        .pluck(:revision_uuid)
+
+      original_item_revisions.each do |revision_uuid|
+        ItemRevision.create(item_uuid: item_id, revision_uuid: revision_uuid)
+      end
     end
   end
 end
