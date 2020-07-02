25f1ae26068ffb5faecc3a303b1d7271dbb0d6cc
Karol Sójko <karolsojko@gmail.com>
2020-07-02 15:21:22 +0200

    fix: Running concurrent migrations in Rails 5.1.7

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,11 +2,23 @@
 set -e
 
 case "$1" in
+  'start-local' )
+    echo "Prestart Step 1/3 - Removing server lock"
+    rm -f /syncing-server/tmp/pids/server.pid
+    echo "Prestart Step 2/3 - Install dependencies"
+    bundle install
+    echo "Prestart Step 3/3 - Migrating database"
+    # bundle exec rails db:migrate
+    bundle exec rake db:migrate:ignore_concurrent
+    echo "Starting Server..."
+    bundle exec rails server -b 0.0.0.0
+    ;;
+
   'start-web' )
     echo "Prestart Step 1/2 - Removing server lock"
     rm -f /syncing-server/tmp/pids/server.pid
     echo "Prestart Step 2/2 - Migrating database"
-    bundle exec rails db:migrate
+    bundle exec rake db:migrate:ignore_concurrent
     echo "Starting Server..."
     bundle exec rails server -b 0.0.0.0
     ;;
