3259fcb3af4cf3e5005c1befb1385ac13f8a9b24
Johnny Almonte <johnny243@users.noreply.github.com>
2020-07-20 10:11:25 -0400

    fix: server history response

--- app/controllers/api/revisions_controller.rb
+++ app/controllers/api/revisions_controller.rb
@@ -1,6 +1,15 @@
 class Api::RevisionsController < Api::ApiController
   def show
-    revision = current_user.items.find(params[:item_id]).revisions.find(params[:id])
+    begin
+      item = current_user.items.find(params[:item_id])
+    rescue ActiveRecord::RecordNotFound
+      return render json: { error: { message: 'Item not found' } }, status: :not_found
+    end
+
+    revision = item.revisions
+      .where(uuid: params[:id])
+      .select('revisions.*')
+      .first
 
     render json: revision
   end
@@ -12,6 +21,10 @@ class Api::RevisionsController < Api::ApiController
       return render json: { error: { message: 'Item not found' } }, status: :not_found
     end
 
-    render json: item.revisions.where(created_at: User::REVISIONS_RETENTION_DAYS.days.ago..DateTime::Infinity.new)
+    revisions = item.revisions
+      .where(created_at: User::REVISIONS_RETENTION_DAYS.days.ago..DateTime::Infinity.new)
+      .select('revisions.uuid, content_type, created_at, updated_at')
+
+    render json: revisions
   end
 end
--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,4 +1,24 @@
 class Revision < ApplicationRecord
   has_many :item_revisions, foreign_key: 'revision_uuid', dependent: :destroy
   has_many :items, through: :item_revisions
+
+  def serializable_hash(options = {})
+    allowed_options = [
+      'uuid',
+      'items_key_id',
+      'duplicate_of',
+      'enc_item_key',
+      'content',
+      'content_type',
+      'auth_hash',
+      'deleted',
+      'created_at',
+      'updated_at',
+    ]
+
+    super(options.merge(only: allowed_options)).merge({
+      revision_uuid: uuid,
+      uuid: item_ids.first,
+    })
+  end
 end
