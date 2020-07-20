b284830d153ce3b65a9279ad565cc7bd578e25e5
Karol Sójko <karolsojko@gmail.com>
2020-07-20 14:38:10 +0200

    Add spec for performing daily backup jobs

--- spec/controllers/admin/admin_controller_spec.rb
+++ spec/controllers/admin/admin_controller_spec.rb
@@ -30,4 +30,29 @@ RSpec.describe Admin::AdminController, type: :controller do
       expect(User.where(email: test_registration[:user][:email])).to_not be_present
     end
   end
+
+  describe 'POST admin/perform_daily_backup_jobs' do
+    before(:all) do
+      ENV['ADMIN_KEY'] = 'secret_admin_key'
+    end
+
+    let(:test_user) do
+      build(:user, password: '123456')
+    end
+
+    it 'should throw unauthorized if not admin_key is not valid' do
+      post :perform_daily_backup_jobs, params: { admin_key: 'something_else' }
+      expect(response).to have_http_status(:unauthorized)
+      expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
+      expect(JSON.parse(response.body)).to eq({})
+    end
+
+    it 'should perform daily backup jobs' do
+      create(:item, :backup_daily, user_uuid: test_user.uuid)
+
+      expect do
+        post :perform_daily_backup_jobs, params: { admin_key: ENV['ADMIN_KEY'] }
+      end.to have_enqueued_job(ExtensionJob)
+    end
+  end
 end
