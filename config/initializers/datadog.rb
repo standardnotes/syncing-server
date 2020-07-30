566973594501cc477263eb4aba0988363eea2d80
Karol Sójko <karolsojko@gmail.com>
2020-07-30 09:36:51 +0200

    fix: datadog service name

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,6 @@
 if ENV['DATADOG_ENABLED'] == 'true'
   Datadog.configure do |c|
     # This will activate auto-instrumentation for Rails
-    c.use :rails
+    c.use :rails, { 'service_name' => 'syncing-server' }
   end
 end
