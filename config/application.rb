c3b37f3cd34c418866b827a74065d08508b6a934
Karol Sójko <karolsojko@gmail.com>
2020-07-30 11:20:50 +0200

    fix: application namespace for datadog trace collection

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
 
-module StandardNotes
+module SyncingServer
   class Application < Rails::Application
     config.load_defaults 5.1
     config.autoload_once_paths += Dir["#{config.root}/lib/**/*"]
--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,10 +1,5 @@
 if ENV['DATADOG_ENABLED'] == 'true'
   Datadog.configure do |c|
-    c.use :rails,
-          {
-            'service_name' => 'syncing-server',
-            'controller_service' => 'syncing-server',
-            'database_service' => 'syncing-server-mysql'
-          }
+    c.use :rails
   end
 end
