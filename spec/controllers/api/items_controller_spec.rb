3dc9b7e6597f1c3eccd7d7f8d443315d12de8f81
Johnny Almonte <johnny243@users.noreply.github.com>
2020-09-30 15:35:28 -0400

    test: for api version 20161215

--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -116,6 +116,7 @@ RSpec.describe Api::ItemsController, type: :controller do
             items_param = serialize_to_hash(items_param)
             expect(saved_items).to match_array(items_param)
           end
+
           it 'should store revisions matching changes' do
             @controller = Api::AuthController.new
             post :sign_in, params: test_user_003_credentials
@@ -382,24 +383,92 @@ RSpec.describe Api::ItemsController, type: :controller do
       end
 
       context 'when using the fallback api' do
-        it 'should return results' do
-          @controller = Api::AuthController.new
-          post :sign_in, params: test_user_003_credentials
+        context 'and a sync request is sent' do
+          it 'should return results' do
+            @controller = Api::AuthController.new
+            post :sign_in, params: test_user_003_credentials
 
-          @controller = Api::ItemsController.new
-          request.headers['Authorization'] = "bearer #{JSON.parse(response.body)['token']}"
-          post :sync, params: { sync_token: '', cursor_token: '', limit: 5, items: [test_items] }
+            @controller = Api::ItemsController.new
+            request.headers['Authorization'] = "bearer #{JSON.parse(response.body)['token']}"
+            post :sync, params: { sync_token: '', cursor_token: '', limit: 5, items: [test_items] }
 
-          expect(response).to have_http_status(:ok)
-          expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
+            expect(response).to have_http_status(:ok)
+            expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
 
-          parsed_response_body = JSON.parse(response.body)
+            parsed_response_body = JSON.parse(response.body)
 
-          expect(parsed_response_body).to_not be_nil
-          expect(parsed_response_body['retrieved_items']).to_not be_nil
-          expect(parsed_response_body['saved_items']).to_not be_nil
-          expect(parsed_response_body['sync_token']).to_not be_nil
-          expect(parsed_response_body).to have_key('cursor_token')
+            expect(parsed_response_body).to_not be_nil
+            expect(parsed_response_body['retrieved_items']).to_not be_nil
+            expect(parsed_response_body['saved_items']).to_not be_nil
+            expect(parsed_response_body['sync_token']).to_not be_nil
+            expect(parsed_response_body).to have_key('cursor_token')
+          end
+        end
+
+        context 'and syncing items without the items_key_id field' do
+          it 'should set a default value for the field' do
+            @controller = Api::AuthController.new
+            post :sign_in, params: test_user_003_credentials
+
+            @controller = Api::ItemsController.new
+            request.headers['Authorization'] = "bearer #{JSON.parse(response.body)['token']}"
+
+            new_item = create(:item, :with_items_key_id, user_uuid: test_user.uuid)
+
+            items_param = [new_item].to_a.map(&:serializable_hash)
+            items_param[0].delete('items_key_id')
+
+            post :sync, params: { limit: 5, items: items_param }
+
+            expect(response).to have_http_status(:ok)
+            expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
+
+            parsed_response_body = JSON.parse(response.body)
+
+            expect(parsed_response_body).to_not be_nil
+
+            saved_items = parsed_response_body['saved_items']
+            expect(saved_items).to_not be_nil
+
+            saved_item = serialize_to_hash(saved_items)[0]
+            actual_item = Item.find(new_item[:uuid])
+
+            expect(saved_item[:items_key_id]).to be_nil
+            expect(actual_item[:items_key_id]).to be_nil
+          end
+        end
+
+        context 'and syncing items without the auth_hash field' do
+          it 'should set a default value for the field' do
+            @controller = Api::AuthController.new
+            post :sign_in, params: test_user_003_credentials
+
+            @controller = Api::ItemsController.new
+            request.headers['Authorization'] = "bearer #{JSON.parse(response.body)['token']}"
+
+            new_item = create(:item, :with_auth_hash, user_uuid: test_user.uuid)
+
+            items_param = [new_item].to_a.map(&:serializable_hash)
+            items_param[0].delete('auth_hash')
+
+            post :sync, params: { limit: 5, items: items_param }
+
+            expect(response).to have_http_status(:ok)
+            expect(response.headers['Content-Type']).to eq('application/json; charset=utf-8')
+
+            parsed_response_body = JSON.parse(response.body)
+
+            expect(parsed_response_body).to_not be_nil
+
+            saved_items = parsed_response_body['saved_items']
+            expect(saved_items).to_not be_nil
+
+            saved_item = serialize_to_hash(saved_items)[0]
+            actual_item = Item.find(new_item[:uuid])
+
+            expect(saved_item[:auth_hash]).to be_nil
+            expect(actual_item[:auth_hash]).to be_nil
+          end
         end
       end
 
