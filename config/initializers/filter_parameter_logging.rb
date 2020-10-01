11bdf75c2f30ce6f0402c9b07a6aa2b86024a63c
Karol Sójko <karolsojko@gmail.com>
2020-10-01 09:56:21 +0200

    fix: remove items content from params logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :content, :enc_item_key, :auth_hash, :admin_key]
+Rails.application.config.filter_parameters += [:password, :content, :enc_item_key, :auth_hash, :admin_key, :items]
