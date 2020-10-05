751f177c0dedb9386366f95b3d89b26dc648928d
Johnny Almonte <johnny243@users.noreply.github.com>
2020-10-05 10:51:23 -0400

    fix: first remove index, then rename columns

--- db/migrate/20201004040050_change_tokens_to_hashed.rb
+++ db/migrate/20201004040050_change_tokens_to_hashed.rb
@@ -1,7 +1,7 @@
 class ChangeTokensToHashed < ActiveRecord::Migration[5.1]
   def change
+    remove_index :sessions, column: [:access_token, :refresh_token]
     rename_column :sessions, :access_token, :hashed_access_token
     rename_column :sessions, :refresh_token, :hashed_refresh_token
-    remove_index :sessions, column: [:hashed_access_token, :hashed_refresh_token]
   end
 end
