d8ccbac33564ad19f2d1ea596380a2167353d152
Johnny Almonte <johnny243@users.noreply.github.com>
2020-10-05 10:33:35 -0400

    fix: remove indexes in a single statement

--- db/migrate/20201004040050_change_tokens_to_hashed.rb
+++ db/migrate/20201004040050_change_tokens_to_hashed.rb
@@ -2,7 +2,6 @@ class ChangeTokensToHashed < ActiveRecord::Migration[5.1]
   def change
     rename_column :sessions, :access_token, :hashed_access_token
     rename_column :sessions, :refresh_token, :hashed_refresh_token
-    remove_index :sessions, name: :hashed_access_token
-    remove_index :sessions, name: :hashed_refresh_token
+    remove_index :sessions, column: [:hashed_access_token, :hashed_refresh_token]
   end
 end
