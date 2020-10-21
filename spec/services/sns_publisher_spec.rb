942935471085ce8cb9a65fe88b5cf1c3722f9ee3
Karol Sójko <karolsojko@gmail.com>
2020-10-21 12:49:25 +0200

    fix: stubbing sns client

--- spec/services/sns_publisher_spec.rb
+++ spec/services/sns_publisher_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 require 'aws-sdk-sns'
 
 RSpec.describe SnsPublisher do
+  let(:stub_client) do
+    Aws::SNS::Client.new(stub_responses: true)
+  end
+
   subject do
     described_class.new
   end
@@ -10,13 +14,15 @@ RSpec.describe SnsPublisher do
     before do
       allow(ENV).to receive(:fetch)
       allow(ENV).to receive(:[])
+      expect(Aws::SNS::Client).to receive(:new).and_return(stub_client)
     end
+
     context 'sns topic not configured' do
       before do
         allow(ENV).to receive(:fetch).with('SNS_TOPIC_ARN', nil).and_return(nil)
       end
       it 'should not publish events' do
-        expect_any_instance_of(Aws::SNS::Client).not_to receive(:publish)
+        expect(stub_client).not_to receive(:publish)
 
         subject.publish_mail_backup_attachment_too_big('test@test.com', 1.megabyte)
       end
@@ -26,7 +32,7 @@ RSpec.describe SnsPublisher do
         allow(ENV).to receive(:fetch).with('SNS_TOPIC_ARN', nil).and_return('test-arn')
       end
       it 'should publish events' do
-        expect_any_instance_of(Aws::SNS::Client).to receive(:publish)
+        expect(stub_client).to receive(:publish)
 
         subject.publish_mail_backup_attachment_too_big('test@test.com', 1.megabyte)
       end
