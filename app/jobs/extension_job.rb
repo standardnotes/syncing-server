725068759008e806631bad6cc83e55b8f08a9148
Karol Sójko <karolsojko@gmail.com>
2020-10-22 08:45:34 +0200

    Add support for using database read replicas (#128)
    * feat: add db read replica support
    
    * feat: add distributed reads to duplicate revisions and extensions job
    
    * fix: remove temporary branch deployment to dev

--- app/jobs/extension_job.rb
+++ app/jobs/extension_job.rb
@@ -5,61 +5,63 @@ class ExtensionJob < ApplicationJob
   require 'uri'
 
   def perform(user_id, url, extension_id, item_ids = [], force_mute = false)
-    user = User.find_by_uuid(user_id)
+    distribute_reads do
+      user = User.find_by_uuid(user_id)
 
-    return if user.nil?
+      return if user.nil?
 
-    items = if item_ids.length.positive?
-      user.items.find(item_ids)
-    else
-      user.items.where(deleted: false).to_a
-    end
+      items = if item_ids.length.positive?
+        user.items.find(item_ids)
+      else
+        user.items.where(deleted: false).to_a
+      end
 
-    auth_params = user.key_params
+      auth_params = user.key_params
 
-    settings = ExtensionSetting.find_or_create_by(extension_id: extension_id)
-    mute_emails = force_mute || settings.mute_emails
+      settings = ExtensionSetting.find_or_create_by(extension_id: extension_id)
+      mute_emails = force_mute || settings.mute_emails
 
-    tmp_file = prepare_tmp_file(auth_params, items)
-    filename = upload_tmp_file_to_s3(tmp_file.path)
-    tmp_file.close
-    tmp_file.unlink
+      tmp_file = prepare_tmp_file(auth_params, items)
+      filename = upload_tmp_file_to_s3(tmp_file.path)
+      tmp_file.close
+      tmp_file.unlink
 
-    payload = {
-      items: items,
-      backup_filename: filename,
-      user_uuid: user.uuid,
-      auth_params: auth_params,
-      silent: mute_emails,
-      settings_id: settings.uuid,
-    }.to_json
+      payload = {
+        items: items,
+        backup_filename: filename,
+        user_uuid: user.uuid,
+        auth_params: auth_params,
+        silent: mute_emails,
+        settings_id: settings.uuid,
+      }.to_json
 
-    begin
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      req = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-      req.body = payload
-      http.use_ssl = (uri.scheme == 'https')
-    rescue StandardError => e
-      Rails.logger.error "Error creating extensions server request: #{e.message}"
-      return
-    end
+      begin
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        req = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+        req.body = payload
+        http.use_ssl = (uri.scheme == 'https')
+      rescue StandardError => e
+        Rails.logger.error "Error creating extensions server request: #{e.message}"
+        return
+      end
 
-    sent = false
-    begin
-      response = http.request(req)
-      sent = response.code.starts_with?('2')
-    rescue StandardError => e
-      Rails.logger.error "Failed to send a request to extensions server: #{e.message}"
-      Rails.logger.info "Response code was #{response.code}. Body sample: #{response.body[0, 100]}" if response
-    end
+      sent = false
+      begin
+        response = http.request(req)
+        sent = response.code.starts_with?('2')
+      rescue StandardError => e
+        Rails.logger.error "Failed to send a request to extensions server: #{e.message}"
+        Rails.logger.info "Response code was #{response.code}. Body sample: #{response.body[0, 100]}" if response
+      end
 
-    unless sent
-      UserMailer.failed_backup(
-        user_id,
-        extension_id,
-        settings.uuid
-      ).deliver_now unless mute_emails
+      unless sent
+        UserMailer.failed_backup(
+          user_id,
+          extension_id,
+          settings.uuid
+        ).deliver_now unless mute_emails
+      end
     end
   rescue StandardError => e
     Rails.logger.error "Could not perform extension job: #{e.message}"
