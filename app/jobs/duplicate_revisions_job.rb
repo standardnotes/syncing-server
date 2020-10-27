5e96b3181de17821307c925856116bde04b4234e
Karol Sójko <karolsojko@gmail.com>
2020-10-27 13:52:22 +0100

    fix: add reconnecting to read replicas on lag failover

--- app/jobs/duplicate_revisions_job.rb
+++ app/jobs/duplicate_revisions_job.rb
@@ -1,28 +1,28 @@
 class DuplicateRevisionsJob < ApplicationJob
+  distribute_reads(max_lag: ENV['DB_REPLICA_MAX_LAG'], lag_failover: ENV['DB_REPLICA_LAG_FAILOVER'] || true)
+
   queue_as ENV['SQS_QUEUE_LOW_PRIORITY'] || 'sn_main_low_priority'
 
   def perform(item_id)
-    distribute_reads do
-      item = Item.find_by_uuid(item_id)
+    item = Item.find_by_uuid(item_id)
 
-      unless item
-        Rails.logger.warn "Could not find item with uuid #{item_id}"
+    unless item
+      Rails.logger.warn "Could not find item with uuid #{item_id}"
 
-        return
-      end
+      return
+    end
 
-      existing_original_item = Item
-        .where(uuid: item.duplicate_of, user_uuid: item.user_uuid)
-        .first
+    existing_original_item = Item
+      .where(uuid: item.duplicate_of, user_uuid: item.user_uuid)
+      .first
 
-      if existing_original_item
-        original_item_revisions = existing_original_item
-          .item_revisions
-          .pluck(:revision_uuid)
+    if existing_original_item
+      original_item_revisions = existing_original_item
+        .item_revisions
+        .pluck(:revision_uuid)
 
-        original_item_revisions.each do |revision_uuid|
-          ItemRevision.create(item_uuid: item_id, revision_uuid: revision_uuid)
-        end
+      original_item_revisions.each do |revision_uuid|
+        ItemRevision.create(item_uuid: item_id, revision_uuid: revision_uuid)
       end
     end
   rescue StandardError => e
