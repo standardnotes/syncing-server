1a38f38d6cd1021eea3f9b596ffea0a3b28a7517
Karol Sójko <karolsojko@gmail.com>
2020-10-29 08:19:02 +0100

    Revert "feat: read revisions from the database read replica (#130)"
    This reverts commit c3ae4f447a14fe434f68f129fc7ce856b4d865df.

--- app/controllers/api/revisions_controller.rb
+++ app/controllers/api/revisions_controller.rb
@@ -1,36 +1,32 @@
 class Api::RevisionsController < Api::ApiController
   def show
-    distribute_reads do
-      begin
-        item = current_user.items.find(params[:item_id])
-      rescue ActiveRecord::RecordNotFound
-        return render json: { error: { message: 'Item not found' } }, status: :not_found
-      end
-
-      revision = item.revisions
-        .where(uuid: params[:id])
-        .select('revisions.*')
-        .order(created_at: :desc)
-        .first
-
-      render json: revision
+    begin
+      item = current_user.items.find(params[:item_id])
+    rescue ActiveRecord::RecordNotFound
+      return render json: { error: { message: 'Item not found' } }, status: :not_found
     end
+
+    revision = item.revisions
+      .where(uuid: params[:id])
+      .select('revisions.*')
+      .order(created_at: :desc)
+      .first
+
+    render json: revision
   end
 
   def index
-    distribute_reads do
-      begin
-        item = current_user.items.find(params[:item_id])
-      rescue ActiveRecord::RecordNotFound
-        return render json: { error: { message: 'Item not found' } }, status: :not_found
-      end
-
-      revisions = item.revisions
-        .where(created_at: User::REVISIONS_RETENTION_DAYS.days.ago..DateTime::Infinity.new)
-        .select('revisions.uuid, content_type, created_at, updated_at')
-        .order(created_at: :desc)
-
-      render json: revisions
+    begin
+      item = current_user.items.find(params[:item_id])
+    rescue ActiveRecord::RecordNotFound
+      return render json: { error: { message: 'Item not found' } }, status: :not_found
     end
+
+    revisions = item.revisions
+      .where(created_at: User::REVISIONS_RETENTION_DAYS.days.ago..DateTime::Infinity.new)
+      .select('revisions.uuid, content_type, created_at, updated_at')
+      .order(created_at: :desc)
+
+    render json: revisions
   end
 end
