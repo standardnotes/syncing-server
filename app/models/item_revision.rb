451426fc971bd40da7cd797d4142e7f41cb7d99c
Karol Sójko <karolsojko@gmail.com>
2020-10-29 15:25:06 +0100

    fix: remove replicated models

--- app/models/item_revision.rb
+++ app/models/item_revision.rb
@@ -1,6 +1,4 @@
 class ItemRevision < ApplicationRecord
-  replicated_model
-
   belongs_to :item, foreign_key: 'item_uuid'
   belongs_to :revision, foreign_key: 'revision_uuid'
 end
--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,6 +1,4 @@
 class Revision < ApplicationRecord
-  replicated_model
-
   has_many :item_revisions, foreign_key: 'revision_uuid', dependent: :destroy
   has_many :items, through: :item_revisions
 
