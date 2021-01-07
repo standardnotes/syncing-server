be3d482c0559c6617b1ffa01a104d5a7055e2df7
Karol Sójko <karolsojko@gmail.com>
2021-01-07 14:00:18 +0100

    feat: add handling revoked sessions

---
+++ app/models/archived_session.rb
@@ -0,0 +1,11 @@
+class ArchivedSession < ApplicationRecord
+  replicated_model
+
+  belongs_to :user, foreign_key: 'user_uuid'
+
+  def self.from_token(request_token)
+    _version, session_id, _access_token = Session.deconstruct_token(request_token)
+
+    ArchivedSession.find_by_uuid(session_id)
+  end
+end
