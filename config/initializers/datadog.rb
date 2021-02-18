d42d176d3056de63065b2fc91c780e64cfa9040b
Karol Sójko <karolsojko@gmail.com>
2021-02-18 10:21:38 +0100

    fix: reduce Datadog trace ingestion to 10% - applied to web and worker

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,5 +2,13 @@ if ENV['DATADOG_ENABLED'] == 'true'
   Datadog.configure do |c|
     c.use :rails
     c.use :shoryuken, service_name: 'syncing-server-worker'
+    c.tracer sampler: Datadog::PrioritySampler.new(
+      post_sampler: Datadog::Sampling::RuleSampler.new(
+          [
+              # Sample all 'syncing-server' traces at 10.00%:
+              Datadog::Sampling::SimpleRule.new(service: 'syncing-server', sample_rate: 0.1000)
+          ]
+      )
+    )
   end
 end
